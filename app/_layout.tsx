7edaa8485c7f55dfa6016a00cfd6930bbfe6997c
James Muscat <jamesremuscat@gmail.com>
2025-06-17 13:10:31 +0100

    Allow screen rotation.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,29 @@
 import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as Orientation from 'expo-screen-orientation';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import '@timing71/services';
 
 import { ServiceContextProvider } from '@/components/serviceHost/context';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  useEffect(
+    () => {
+      const unlockScreenOrientation = async () => {
+        await Orientation.unlockAsync()
+      }
+      unlockScreenOrientation()
+    },
+    []
+  );
 
   if (!loaded) {
     // Async font loading only occurs in development.
